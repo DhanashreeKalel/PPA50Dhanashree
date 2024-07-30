dc6049249d39ae8b942342b1960775ec39579883
Dhanashree Sanjay Kalel <dbmore1127@gmail.com>
2024-07-30 20:11:31 +0530

    Add files via upload

--- Generic.c
+++ Generic.c
@@ -2,32 +2,31 @@
 
 int main()
 {
-    char ch='A';
-    int i=11;
-    float f=90.99;
-    double d=90.56789;
+    char ch = 'A';
+    int i = 11;
+    float f = 90.99;
+    double d = 90.56789;
 
-    char *cp=&ch;
-    int *ip=&i;
-    float *fp=&f;
-    double *dp=&d;
+    char *cp = &ch;
+    int *ip = &i;
+    float *fp = &f;
+    double *dp = &d;
 
-    void *vp=NULL;
+    void *vp = NULL;
 
     printf("%c\n",*cp);
-    printf("%d\n",*ip);
+    printf("%d\n",*ip);    
     printf("%f\n",*fp);
     printf("%f\n",*dp);
 
-    vp=&ch;
-    printf("%c\n",*(char *)cp);
-    vp=&i;
-    printf("%d\n",*(int *)ip);
-    vp=&f;
+    vp = &ch;
+    printf("%c\n",*(char *)vp);
+    vp = &i;
+    printf("%d\n",*(int *)vp);    
+    vp = &f;
     printf("%f\n",*(float *)vp);
-    vp=&d;
-    printf("%f\n",*(double *)dp);
-
+    vp = &d;
+    printf("%f\n",*(double *)vp);
 
     return 0;
 }
